a073d01f145062af9f5355c0d775809a9a1103a3
Mateo Blanco <mateoblanco3@gmail.com>
2019-02-26 14:44:39 -0300

    no message

---
+++ precache-manifest.014b7af30154a825dabfc2c5403eeb11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9832cdaac38f2d2cd473",
+    "url": "/citinfo/static/js/runtime~main.9832cdaa.js"
+  },
+  {
+    "revision": "29eb6678c70820ef03a3",
+    "url": "/citinfo/static/js/main.29eb6678.chunk.js"
+  },
+  {
+    "revision": "63f22129a5ab0b8a476b",
+    "url": "/citinfo/static/js/2.63f22129.chunk.js"
+  },
+  {
+    "revision": "29eb6678c70820ef03a3",
+    "url": "/citinfo/static/css/main.cb0b73b4.chunk.css"
+  },
+  {
+    "revision": "63f22129a5ab0b8a476b",
+    "url": "/citinfo/static/css/2.5b984b46.chunk.css"
+  },
+  {
+    "revision": "f37a18e3fbe34059905382331f6b28e1",
+    "url": "/citinfo/index.html"
+  }
+];
